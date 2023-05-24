45ac88a8cf5d9de6f4040ae65747f2fd5e266262
Rus <gerasimchuk91@gmail.com>
2023-05-24 15:07:53 +0300

    2

--- js/script.js
+++ js/script.js
@@ -284,10 +284,17 @@ document.addEventListener('DOMContentLoaded', () => {
         audio.play();
         target.classList.remove('play-btn')
         target.classList.add('pause-btn')
+
+
         return
       })
     })
   }
+
+
+
+
+
 
   // Progress bar
   function updateProgress(e) {
@@ -311,6 +318,11 @@ document.addEventListener('DOMContentLoaded', () => {
       if (sec == 60) {
         sec = 0
       }
+    }
+
+    if (currentTime == duration) {
+      console.log('the end')
+      document.querySelector('.next-btn').click()
     }
 
 
@@ -463,6 +475,8 @@ document.addEventListener('DOMContentLoaded', () => {
       const popup = document.querySelector('.popup-tape');
       const popupContainer = popup.querySelector('.popup-tape__container')
       const title = popup.querySelector('.popup-tape__title')
+      const descr = popup.querySelector('.popup-tape__descr.rus')
+      const descrEng = popup.querySelector('.popup-tape__descr.eng')
       const firstImg = popup.querySelector('.popup-tape__img_1')
       const secondImg = popup.querySelector('.popup-tape__img_2')
 
@@ -470,11 +484,25 @@ document.addEventListener('DOMContentLoaded', () => {
       firstImg.src = `${source}-tape1.jpg`
       secondImg.src = `${source}-tape2.jpg`
 
+      if (source.includes('obl1')) {
+        descr.innerHTML = `Дымчатый матовый кейс, зашкуренный вручную корпус с тампопечатью.`;
+        descrEng.innerHTML = `Smoky box, hand-sanded case with pad printing.`
+      } else if (source.includes('obl2')) {
+        descr.innerHTML = `Вторичное использование советских кассет типа "МК" с новой магнитной пленкой, самодельные обложки, ксерокс с элемантами коллажа.`;
+        descrEng.innerHTML = `Recycle soviet tapes "MK" with modern magnetic tape. DIY covers, xerox with collage elements.`
+      } else if (source.includes('obl3')) {
+        descr.innerHTML = `Вторичное использование советских кассет типа "МК" с новой магнитной пленкой, самодельные обложки, ксерокс с эелемантами коллажа.`;
+        descrEng.innerHTML = `Recycle soviet tapes "MK" with modern magnetic tape. DIY covers, xerox with collage elements.`
+      } else if (source.includes('obl4')) {
+        descr.innerHTML = `Вторичное использование советских кассет типа "МК" с новой магнитной пленкой, печать плоская, бумага неустановленная,художественное оформление и печать сделаны издательством «Подклет» г. Червоград.`;
+        descrEng.innerHTML = `Recycle soviet tapes "MK" with modern magnetic tape,  flat printing, unknown paper, art and print by Podklet publishing house in Chervograd.`
+      }
+
       popup.classList.add('is-open')
       popupContainer.classList.add('is-open')
 
       popup.addEventListener('click', (e) => {
-        if (!e.target.closest('.popup-tape__img')) {
+        if (!e.target.closest('.popup-tape__img') && !e.target.closest('.popup-tape__descr') && !e.target.closest('.popup-tape__for-sale')) {
           popup.classList.remove('is-open')
           popupContainer.classList.remove('is-open')
           document.querySelector('body').classList.remove('overflow')
@@ -499,9 +527,9 @@ document.addEventListener('DOMContentLoaded', () => {
         elem.classList.remove('display-none')
       })
 
-      for (let instance of instances) {
-        instance.setContent('Скопировать в буфер обмена')
-      }
+      // for (let instance of instances) {
+      //   instance.setContent('Скопировать в буфер обмена')
+      // }
     }
   })
 
@@ -515,15 +543,14 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelectorAll('.eng').forEach((elem) => {
         elem.classList.remove('display-none')
       })
-      for (let instance of instances) {
-        instance.setContent('Copy to clipboard')
-      }
+      // for (let instance of instances) {
+      //   instance.setContent('Copy to clipboard')
+      // }
 
     }
   })
 
   function popupDonation(btn) {
-    console.log('xmmm')
     let popup = document.querySelector('.popup-donation');
     let container = popup.querySelector('.popup-donation__container');
 
@@ -561,35 +588,46 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 
+  // popup-video
+  document.querySelector('.footer__video-btn').addEventListener('click', () => {
+    const popup = document.querySelector('.popup-video')
+    const container = document.querySelector('.popup-video__container')
+    popup.classList.add('is-open')
+
+    popup.addEventListener('click', (e) => {
+      if (!e.target.closest('.popup-donation__container')) {
+        popup.classList.remove('is-open')
+        container.classList.remove('is-open')
+
+      }
+    })
+  })
+
+  //  Copy to clipboard
+
+  // document.querySelectorAll('.popup-donation__value').forEach((elem) => {
+  //   elem.addEventListener('click', (e) => {
+  //     const target = e.target.closest('.popup-donation__value')
+  //     const value = target.textContent
+  //     navigator.clipboard.writeText(value)
+
+
+  //   })
+  // })
+
 
   // tooltip
 
-  tippy('.popup-donation__value', {
-    content: 'Скопировать в буфер обмена'
-  })
+  // tippy('.popup-donation__value', {
+  //   content: 'Скопировать в буфер обмена'
+  // })
 
-  const instances = []
-  document.querySelectorAll('.popup-donation__value').forEach((el) => {
-    instance = el._tippy
-    instances.push(instance)
-  })
-  console.log(instances)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // const instances = []
+  // document.querySelectorAll('.popup-donation__value').forEach((el) => {
+  //   instance = el._tippy
+  //   instances.push(instance)
+  // })
+  // console.log(instances)
 
 
 
